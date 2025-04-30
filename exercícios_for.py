cae93f44d570541ff575db48d96970ce559acf5a
Arthur Herbster <arthur20092020@gmail.com>
2025-04-30 02:38:47 -0300

    Exercícios - (FOR)

---
+++ exercícios_for.py
@@ -0,0 +1,13 @@
+# Exercício 1 – Contar de 1 até n
+
+n = int(input('Digite um número inteiro positivo!'))
+
+for i in range(1, n + 1):
+    print(i)
+
+# Exercício 2 – Tabuada de um número
+
+num = int(input('Digite um número para ver a tabuada'!))
+
+for i in range(1, 11):
+    print(f'{num} x {i} = {num * i}')
